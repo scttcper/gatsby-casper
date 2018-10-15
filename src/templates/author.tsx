48d764b911b9f9aa8a74f64eb25a07f695942a70
Scott Cooper <scttcper@gmail.com>
2018-10-14 19:38:12 -0700

    consolidate icons

--- src/templates/author.tsx
+++ src/templates/author.tsx
@@ -1,4 +1,3 @@
-// tslint:disable:no-http-string
 import { graphql } from 'gatsby';
 import React from 'react';
 import styled, { css } from 'react-emotion';
@@ -24,6 +23,8 @@ import { PageContext } from './post';
 import Facebook from '../components/icons/facebook';
 import Helmet from 'react-helmet';
 import config from '../website-config';
+import Website from '../components/icons/website';
+import Twitter from '../components/icons/twitter';
 
 const HiddenMobile = css`
   @media (max-width: 500px) {
@@ -172,19 +173,7 @@ const Author: React.SFC<AuthorTemplateProps> = props => {
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        style={{ height: '1.6rem' }}
-                      >
-                        <path
-                          d="M23.5 11.957c0 6.375-5.163 11.544-11.532 11.544C5.599 23.5.5 18.125.5 11.75.5 5.542 5.37.758 11.505.511l.5-.011C18.374.5 23.5 5.582 23.5 11.957zM11.505.511c-6 6.5-6 14.98 0 22.98m1-22.98c6 6.5 6 14.977 0 22.977M2 17.479h20.063m-19.657-12h19.062m-20.968 6h22.938"
-                          stroke="#fff"
-                          strokeLinejoin="round"
-                          strokeMiterlimit="10"
-                          fill="none"
-                        />
-                      </svg>
+                      <Website />
                     </a>
                   </div>
                 )}
@@ -195,9 +184,7 @@ const Author: React.SFC<AuthorTemplateProps> = props => {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                      <path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z" />
-                    </svg>
+                    <Twitter />
                   </a>
                 )}
                 {author.facebook && (
