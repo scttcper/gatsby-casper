9c5df31e51ced774f8214b20bc9bd52e2ac6a607
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2018-12-09 21:22:58 -0600

    feature: add Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,5 +62,23 @@ module.exports = {
         postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-XXXX-Y',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // IP anonymization for GDPR compliance
+        anonymize: true,
+        // Disable analytics for users with `Do Not Track` enabled
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+        // Specifies what percentage of users should be tracked
+        sampleRate: 100,
+        // Determines how often site speed tracking beacons will be sent
+        siteSpeedSampleRate: 10,
+      },
+    },
   ],
 };
