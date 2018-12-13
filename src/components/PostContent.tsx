1326c8f48e46fed02b682e7031cfb56c8b2671a4
Tony Ho <tony-ho@users.noreply.github.com>
2018-12-13 22:31:18 +1100

    Update text colours to meet WCAG 2.0 AA contrast ratio thresholds

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -135,7 +135,7 @@ export const PostFullContent = styled.section`
   blockquote {
     margin: 0 0 1.5em;
     padding: 0 1.5em;
-    border-left: #3eb0ef 3px solid;
+    border-left: ${colors.blue} 3px solid;
   }
 
   blockquote p {
