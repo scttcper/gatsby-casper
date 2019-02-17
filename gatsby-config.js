348a432d72b152f21b840f3ed03ccea3451c3940
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2019-02-17 15:05:04 +0200

    Replace analytics with GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,21 +77,13 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-tagmanager`,
       options: {
-        trackingId: 'UA-28844594-2',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
+        id: "GTM-KGNHVDJ",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false
       },
     },
   ],
