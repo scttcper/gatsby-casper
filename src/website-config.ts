3c50a6b875dfa26e7d4d8a299ce6d5c036dc151c
Valentino Urbano <valeIT@users.noreply.github.com>
2019-05-02 18:57:10 +0200

    Added optional footer (#41)
    * Make facebook optional
    
    * Dynamic copyright year
    
    * merge upstream #2
    
    * footer config
    
    * merge upstream
    
    * merge upstream
    
    * Added basic footer text
    
    * Added comments

--- src/website-config.ts
+++ src/website-config.ts
@@ -12,7 +12,13 @@ export interface WebsiteConfig {
    * blog full path, no ending slash!
    */
   siteUrl: string;
+  /**
+   * full url, no username
+   */
   facebook?: string;
+  /**
+   * full url, no username
+   */
   twitter?: string;
   /**
    * hide or show all email subscribe boxes
@@ -26,6 +32,10 @@ export interface WebsiteConfig {
    * this is the hidden input field name
    */
   mailchimpName?: string;
+  /** 
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
 }
 
 const config: WebsiteConfig = {
@@ -40,6 +50,7 @@ const config: WebsiteConfig = {
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  footer: 'is based on Gatsby Casper',
 };
 
 export default config;
