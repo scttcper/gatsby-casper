af205de61c296ab9d1b9f6569e397d8ee3c69e7b
Valentino Urbano <valeIT@users.noreply.github.com>
2019-05-07 21:45:45 +0200

    Added Google Meta Tag (#45)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,6 +103,7 @@ const IndexPage: React.FunctionComponent<IndexProps> = props => {
           content={`${config.siteUrl}${props.data.header.childImageSharp.fluid.src}`}
         />
         {config.facebook && <meta property="article:publisher" content={config.facebook} />}
+        {config.googleSiteVerification && <meta name="google-site-verification" content={config.googleSiteVerification} />}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={config.title} />
         <meta name="twitter:description" content={config.description} />
