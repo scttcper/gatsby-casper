a8fe87301c9bbd15c21a9554ae74b4e2249d9cce
Scott Cooper <scttcper@gmail.com>
2019-07-22 19:28:02 -0700

    chore: fix lint

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -47,33 +47,33 @@ const navCss = css`
   }
 `
 
-const Pagination: React.FunctionComponent<PaginationProps> = ({currentPage, numPages}) => {
+const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1;
   const isLast = currentPage === numPages;
-  const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString();
+  const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString();
   const nextPage = (currentPage + 1).toString();
 
   return (
     <nav css={navCss}>
       <div>
         {!isFirst && (
-            <Link to={prevPage} rel="prev">
-              {/* << symbol */}
-              {String.fromCharCode(171)}
-            </Link>
+          <Link to={prevPage} rel="prev">
+            {/* << symbol */}
+            {String.fromCharCode(171)}
+          </Link>
         )}
 
         {Array.from({ length: numPages }, (_, i) => (
-            <Link className={i + 1 === currentPage ? "active" : ""} key={`pagination-number${i + 1}`} to={`/${i === 0 ? "" : i + 1}`}>
-              {i + 1}
-            </Link>
+          <Link key={`pagination-number${i + 1}`} className={i + 1 === currentPage ? 'active' : ''} to={`/${i === 0 ? '' : i + 1}`}>
+            {i + 1}
+          </Link>
         ))}
 
         {!isLast && (
-            <Link to={nextPage} rel="next">
-              {/* >> symbol */}
-              {String.fromCharCode(187)}
-            </Link>
+          <Link to={nextPage} rel="next">
+            {/* >> symbol */}
+            {String.fromCharCode(187)}
+          </Link>
         )}
       </div>
     </nav>
