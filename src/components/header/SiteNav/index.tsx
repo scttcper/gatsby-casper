6b3f115285c10653b596d76e20057cfd58d21689
DoLim <limdongyoung0@gmail.com>
2019-12-30 14:09:26 +0000

    Splitted style from sitenav

--- src/components/header/SiteNav/index.tsx
+++ src/components/header/SiteNav/index.tsx
@@ -1,116 +1,15 @@
 // tslint:disable:no-http-string
 import { Link } from 'gatsby';
 import * as React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 
-import { SocialLink } from '../../../styles/shared';
 import config from '../../../website-config';
 import Facebook from '../../icons/facebook';
 import Twitter from '../../icons/twitter';
 import SubscribeModal from '../../subscribe/SubscribeOverlay';
 import SiteNavLogo from '../SiteNavLogo';
+import { SocialLink } from '../../../styles/shared';
+import {HomeNavRaise, SiteNavStyles, SiteNavLeft, SiteNavRight, NavStyles, SocialLinks, SubscribeButton} from './style';
 
-const HomeNavRaise = css`
-  @media (min-width: 900px) {
-    position: relative;
-    top: -70px;
-  }
-`;
-
-const SiteNavStyles = css`
-  position: relative;
-  z-index: 300;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  overflow-y: hidden;
-  height: 40px;
-  font-size: 1.2rem;
-`;
-
-const SiteNavLeft = styled.div`
-  display: flex;
-  align-items: center;
-  overflow-x: auto;
-  overflow-y: hidden;
-  -webkit-overflow-scrolling: touch;
-  margin-right: 10px;
-  padding-bottom: 80px;
-  letter-spacing: 0.4px;
-  white-space: nowrap;
-
-  -ms-overflow-scrolling: touch;
-
-  @media (max-width: 700px) {
-    margin-right: 0;
-    padding-left: 4vw;
-  }
-`;
-
-const NavStyles = css`
-  display: flex;
-  margin: 0 0 0 -12px;
-  padding: 0;
-  list-style: none;
-
-  li {
-    display: block;
-    margin: 0;
-    padding: 0;
-    text-transform: uppercase;
-  }
-
-  li a {
-    display: block;
-    margin: 0;
-    padding: 10px 12px;
-    color: #fff;
-    opacity: 0.8;
-  }
-
-  li a:hover {
-    text-decoration: none;
-    opacity: 1;
-  }
-`;
-
-const SiteNavRight = styled.div`
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  height: 40px;
-
-  @media (max-width: 700px) {
-    display: none;
-  }
-`;
-
-const SocialLinks = styled.div`
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  a:last-of-type {
-    padding-right: 20px;
-  }
-`;
-
-const SubscribeButton = styled.a`
-  display: block;
-  padding: 4px 10px;
-  border: #fff 1px solid;
-  color: #fff;
-  font-size: 1.2rem;
-  line-height: 1em;
-  border-radius: 10px;
-  opacity: 0.8;
-
-  :hover {
-    text-decoration: none;
-    opacity: 1;
-    cursor: pointer;
-  }
-`;
 
 interface SiteNavProps {
   isHome?: boolean;
---
+++ src/components/header/SiteNav/style.tsx
@@ -0,0 +1,105 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+
+
+export const HomeNavRaise = css`
+  @media (min-width: 900px) {
+    position: relative;
+    top: -70px;
+  }
+`;
+
+export const SiteNavStyles = css`
+  position: relative;
+  z-index: 300;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  overflow-y: hidden;
+  height: 40px;
+  font-size: 1.2rem;
+`;
+
+export const SiteNavLeft = styled.div`
+  display: flex;
+  align-items: center;
+  overflow-x: auto;
+  overflow-y: hidden;
+  -webkit-overflow-scrolling: touch;
+  margin-right: 10px;
+  padding-bottom: 80px;
+  letter-spacing: 0.4px;
+  white-space: nowrap;
+
+  -ms-overflow-scrolling: touch;
+
+  @media (max-width: 700px) {
+    margin-right: 0;
+    padding-left: 4vw;
+  }
+`;
+
+export const NavStyles = css`
+  display: flex;
+  margin: 0 0 0 -12px;
+  padding: 0;
+  list-style: none;
+
+  li {
+    display: block;
+    margin: 0;
+    padding: 0;
+    text-transform: uppercase;
+  }
+
+  li a {
+    display: block;
+    margin: 0;
+    padding: 10px 12px;
+    color: #fff;
+    opacity: 0.8;
+  }
+
+  li a:hover {
+    text-decoration: none;
+    opacity: 1;
+  }
+`;
+
+export const SiteNavRight = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  height: 40px;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const SocialLinks = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  a:last-of-type {
+    padding-right: 20px;
+  }
+`;
+
+export const SubscribeButton = styled.a`
+  display: block;
+  padding: 4px 10px;
+  border: #fff 1px solid;
+  color: #fff;
+  font-size: 1.2rem;
+  line-height: 1em;
+  border-radius: 10px;
+  opacity: 0.8;
+
+  :hover {
+    text-decoration: none;
+    opacity: 1;
+    cursor: pointer;
+  }
+`;
