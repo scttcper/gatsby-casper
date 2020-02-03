8bf2fc843e5c0cd8977474879e317933219dff92
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2020-02-03 11:30:26 +0100

    post card hover effect tweak

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -19,12 +19,10 @@ const PostCardStyles = css`
   background-size: cover;
   border-radius: 5px;
   box-shadow: rgba(39, 44, 49, 0.06) 8px 14px 38px, rgba(39, 44, 49, 0.03) 1px 3px 8px;
-  transition: all 0.5s ease;
 
-  :hover {
-    box-shadow: rgba(39, 44, 49, 0.07) 8px 28px 50px, rgba(39, 44, 49, 0.04) 1px 6px 12px;
-    transition: all 0.4s ease;
-    transform: translate3D(0, -1px, 0) scale(1.02);
+  :hover .post-card-image {
+    transition: all 2s ease;
+    transform: translate3D(0, -1px, 0) scale(1.1);
   }
 `;
 
@@ -40,6 +38,7 @@ const PostCardImage = styled.div`
   height: 200px;
   background: ${colors.lightgrey} no-repeat center center;
   background-size: cover;
+  transition: all 2s ease;
 `;
 
 const PostCardContent = styled.div`
