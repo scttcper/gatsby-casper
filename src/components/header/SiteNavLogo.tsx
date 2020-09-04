0cae670e31cf974771c815506fced35bbbc561a7
IshaqMccann <70007826+IshaqMccann@users.noreply.github.com>
2020-09-04 11:30:39 +0300

    Update SiteNavLogo.tsx

--- src/components/header/SiteNavLogo.tsx
+++ src/components/header/SiteNavLogo.tsx
@@ -17,7 +17,7 @@ export const SiteNavLogo = () => (
   <StaticQuery
     query={graphql`
       query HeadingQuery {
-        logo: file(relativePath: { eq: "img/ghost-logo.png" }) {
+        logo: file(relativePath: { eq: "img/log.png" }) {
           childImageSharp {
             fixed(quality: 100 width: 500) {
               ...GatsbyImageSharpFixed
