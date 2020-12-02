1e1ec673a71002a2078dd21655323fa6cb600e01
Scott Cooper <scttcper@gmail.com>
2020-12-02 07:48:08 -0800

    upgrade dependencies, revert to gatsby emotion plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-typescript',
+    'gatsby-plugin-emotion',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
