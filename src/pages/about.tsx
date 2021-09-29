4af8ae5f43eb382a045f5f24522a614e36104362
alitajalli <51313120+alitajalli@users.noreply.github.com>
2021-09-29 16:06:02 +0330

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -57,43 +57,44 @@ const About: React.FC = () => (
             <PostFullContent className="post-full-content">
               <div className="post-content">
                 <h5>
-                  A starter template for Gatsby <br /> GitHub:{' '}
-                  <a href="https://github.com/scttcper/gatsby-casper">scttcper/gatsby-casper</a>
+                  Green Garnet <br /> GitHub:{' '}
+                  <a > Meanings, Properties and Powers</a>
                 </h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo finibus leo,
-                  non tempus magna vehicula ac. Maecenas mollis ante finibus pharetra imperdiet.
-                  Maecenas in aliquam purus. Nam et massa a nulla fermentum dapibus sit amet in
-                  neque. Ut ipsum ipsum, rhoncus a sodales pellentesque, interdum a elit. Nullam
-                  aliquam tellus nibh, eget laoreet dui aliquet non. Vestibulum malesuada ante at
-                  diam tempus, ac interdum risus scelerisque. Sed ipsum neque, vulputate porta diam
-                  eget, consequat blandit nulla. Integer volutpat velit vitae purus lacinia aliquam.
-                  Integer bibendum ipsum vitae magna pulvinar, nec vehicula dolor vulputate. Nulla
-                  eu massa id orci placerat finibus vel sit amet eros. Vestibulum quis consequat
-                  massa. Sed sagittis sollicitudin massa at commodo. Praesent diam nisi, imperdiet
-                  posuere eleifend nec, blandit ac massa.
+                  Green-Garnet is restorative and liberating. It’s a growth stone that is also a powerful conduit of birth, creation, development, and renewal.
+                  It’s a powerful aid in nurturing a fledgling relationship or a new business endeavor. It will also promote a strengthened commitment to your higher purpose and spiritual growth.
+                  It will bring new strength when you are experiencing a difficult time or a challenging period in your life.
+                  It’s a stone that will cleanse the heart chakra, which will increase your physical vitality and zest for life. It will also induce feelings of kindness, compassion, and charity.
+                  Beautiful Green Garnet beads
+                  Green-Garnet is a known gemstone that has the power of regeneration and growth.
+                  Its beautiful green color is a symbol of Mother Earth.
+                  It’s believed that Green-Garnet has the power to accept, care for, and nurture everything!
+                  It’s a stone that’s brimming with hope, and its powerful energies will definitely brighten your future!
+                  Green-Garnet symbolizes growth and renewal in the different aspects of your life.
                 </p>
                 <p>
-                  Vestibulum semper pretium ipsum nec congue. Ut ac eros nisi. Donec leo sem,
-                  aliquam mollis sapien ultrices, dapibus congue diam. Proin viverra dapibus
-                  blandit. Ut mauris tellus, tristique id felis vel, venenatis vestibulum nunc. Nam
-                  molestie pulvinar nibh, eget egestas augue. Maecenas tellus arcu, mattis ut ipsum
-                  non, sollicitudin convallis nunc. Donec nec neque tristique, aliquet lacus id,
-                  laoreet nunc. Cras dapibus nisi nulla, ullamcorper faucibus neque suscipit ac.
-                  Donec eget orci venenatis justo lobortis volutpat. Proin vel placerat nisl.
-                  Integer arcu nunc, sodales eu fringilla non, aliquam non diam. Cras placerat,
-                  massa et faucibus pretium, ante elit tincidunt tellus, tristique ultricies velit
-                  quam et massa.
+                  Green-Garnet also has physical healing properties that can benefit the body in more ways than one.
+                  This gemstone is known to be very effective in speeding up the recovery period after an illness or physical trauma.
+                  It can stimulate blood flow and help in new cell growth, especially when you pair it with the April Birthstone.
+                  It can also boost the respiratory and the immune systems and protect you against highly infectious diseases.
+                  The healing energies of Green-Garnet can detoxify the body and reduce inflammations
                 </p>
                 <p>
-                  In nunc lacus, dapibus vitae lacus sit amet, efficitur iaculis neque. Suspendisse
-                  ut tellus quis leo vestibulum tincidunt. Aenean nec enim ac dolor lacinia semper.
-                  Ut sed laoreet libero. Nunc elementum sollicitudin accumsan. Nunc eu augue neque.
-                  Proin a tortor nibh. Cras eu nisl ornare sapien feugiat pellentesque. Mauris
-                  dignissim vel quam eu pellentesque. Integer sit amet posuere quam, eu ullamcorper
-                  odio. Nullam a lacus tempus sapien dignissim ullamcorper. In hac habitasse platea
-                  dictumst. Proin quis massa aliquam, feugiat tortor sit amet, tincidunt urna. Donec
-                  posuere pulvinar lectus, ac semper ipsum vulputate quis.
+                  Green-Garnet is the gemstone that will confirm the happiness that you have in your love life.
+                  It will show you how you can appreciate the people you love and the people who love you back.
+                  It also has the ability to strengthen not only the heart but the soul as well. It will help you take in new energies and become whole again 
+                  or a new and better version of yourself.
+                  If you want to make a new beginning, Green-Garnet is the perfect gemstone for you!
+                  Green-Garnet also symbolizes hope. It will give you clarity and help you see things or people in a different light.
+                  It will solidify new relationships and add new meaning to your life and your relationship.
+                  When you work with the energies of this stone, you will feel courageous and confident to make big changes and major decisions.
+                  It’s a gemstone that will encourage gratitude. It will make you appreciate all the good things that you have going for you.
+                  It will even make you thankful for the bad, because without them, you will never realize just how blessed you are!
+                  Green-Garnet will inspire you to be of service to the people you love. You will do things for them without expecting anything in return.
+                  You will do it simply because you love them and you just want to see them happy and okay.
+                  This gemstone will promote a relaxed and laidback attitude in your relationship. It will encourage you to go with the flow and not to be too rigid in your ways.
+                  Green-Garnet will inspire you to inject good old-fashioned romance and silly fun into a serious relationship.
+                  It will remind you to never lose passion or energy for one another, especially during times when you’re filled with stress, fear, or worry.
                 </p>
               </div>
             </PostFullContent>
