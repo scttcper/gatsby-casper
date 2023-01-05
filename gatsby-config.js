7584c205e1109286734d2126c201de70dfb122cb
Michael Asimakopoulos <masimplo@gmail.com>
2023-01-05 19:37:36 +0200

    Add gatsby netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sitemap',
     'gatsby-plugin-image',
+    'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-plugin-sharp',
       options: {
